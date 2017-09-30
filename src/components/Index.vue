cb5db324f4e71294bb7cc8c26e9e60bdf4400e29
syon <syon.xiii@gmail.com>
2017-10-01 00:31:22 +0900

    :walking: Index.vue

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+h1 Index.vue
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
