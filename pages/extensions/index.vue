d9b18537379fb685ee9dc447bea083c515e75323
syon <syon.xiii@gmail.com>
2018-03-03 23:21:55 +0900

    :twisted_rightwards_arrows: routes

--- pages/extensions/index.vue
+++ pages/extensions/index.vue
@@ -3,16 +3,16 @@
     <h1>Extensions</h1>
     <ul>
       <li>
-        <router-link :to="{ path: '/extension/sample.hello-world'}">sample.hello-world</router-link>
+        <router-link :to="{ path: '/extensions/sample.hello-world'}">sample.hello-world</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/extension/alefragnani.project-manager'}">alefragnani.project-manager</router-link>
+        <router-link :to="{ path: '/extensions/alefragnani.project-manager'}">alefragnani.project-manager</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/extension/oderwat.indent-rainbow'}">oderwat.indent-rainbow</router-link>
+        <router-link :to="{ path: '/extensions/oderwat.indent-rainbow'}">oderwat.indent-rainbow</router-link>
       </li>
       <li>
-        <router-link :to="{ path: '/extension/wmaurer.vscode-jumpy'}">wmaurer.vscode-jumpy</router-link>
+        <router-link :to="{ path: '/extensions/wmaurer.vscode-jumpy', query: { plan: 'private' }}">wmaurer.vscode-jumpy</router-link>
       </li>
     </ul>
   </section>
